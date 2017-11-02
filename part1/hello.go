625662b42af22db8b2f9070ce371445a1cc4397c
boseny <a707328818@qq.com>
2017-11-02 22:12:43 +0800

    add

--- part1/hello.go
+++ part1/hello.go
@@ -4,10 +4,15 @@ package是定义包名，表明你这个文件属于哪个包, package main 表�
 */
 package main 
 
-
+/* 
+这里import就是去导入你所要使用的包，这里导入了一个用于打印控制台的fmt包和一个自己写的cal包 
+*/
 import "fmt"
 import "./cal"
 
+/*
+func main() 是一个函数 而且main()是一个程序开始执行的函数，main函数式每一个可执行程序必须包含的
+*/
 func main() {
 	fmt.Println("Hello, 世界")
 	result := cal.Add(1, 2)
---
+++ part1/variables.go
@@ -0,0 +1,9 @@
+package main 
+import "fmt"
+var x, y, z int
+var a, b, c bool
+func main() {
+	
+
+	fmt.Println(x, y, z, a, b, c)
+}
